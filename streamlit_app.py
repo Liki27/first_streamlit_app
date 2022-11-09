9b1ec4169bf555fa1dbfeb66ed02897a6fdc30bb
Liki27 <117819468+Liki27@users.noreply.github.com>
2022-11-09 17:00:55 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('My Breakfast Menu')
+streamlit.text('Orange 3 & Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled, Free-Range, Egg')
